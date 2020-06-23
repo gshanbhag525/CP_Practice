399a79f9ce9e36ed17bb5f89f3f43faad65d1233
gshanbhag525 <17478096+gshanbhag525@users.noreply.github.com>
2020-06-23 11:09:27 +0530

    🔃recursive fibo

---
+++ CourseraAlgoToolBox/Fibo.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int fibo(int n)
+{
+    int f = 0, s = 1, t;
+    int sum = fibo()
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    int res = fibo(n - 1) + fibo(n - 2);
+    cout << "result is " << res;
+
+    return 0;
+}
